e69f118c18be68691933734e9f0c3420ad843ccd
dev-hazi <golaphazi@gmail.com>
2021-04-06 10:23:32 +0600

    compl

--- js/nextMixinJs.js
+++ js/nextMixinJs.js
@@ -11,7 +11,7 @@ class NextMixinJs{
     
     
 
-    loadMixin( $selector = ''){
+    loadMixin( $selector = '', $settings = ''){
         let $el =  document.querySelectorAll($selector);
         if( !$el ){
             return;
@@ -21,14 +21,19 @@ class NextMixinJs{
             $v.classList.add('njs-mixin-wrapper');
             $v.setAttribute('njs-mixin', 'nextmix-'+$k); 
             $v.setAttribute('njs-display', 'desktop'); 
+            if( $settings != ''){
+                $v.setAttribute('njs-settings', JSON.stringify($settings)); 
+            }
 
             NextMixinJs.instance().getSettings($v);
+
+            
+            //get filter
+            NextMixinJs.instance().getFilter($v);
 
             // content settings
             NextMixinJs.instance().getContents($v);
 
-            //get filter
-            NextMixinJs.instance().getFilter($v);
             
         });
         
@@ -52,8 +57,18 @@ class NextMixinJs{
         NextMixinJs.instance().mobileMedia(mobile);
         mobile.addListener( NextMixinJs.instance().mobileMedia);
 
+        // window resize
+        window.addEventListener('resize', NextMixinJs.instance().resizeMedia);
     }
 
+    resizeMedia( e ){
+        var w = window.outerWidth;
+        var h = window.outerHeight;
+
+        document.querySelectorAll('.njs-mixin-wrapper').forEach(function($v){
+            NextMixinJs.instance().getContents($v);
+        });
+    }
     desktopMedia( $x ){
         if($x.matches){
             document.querySelectorAll('.njs-mixin-wrapper[njs-settings]').forEach(function($v){
@@ -161,8 +176,7 @@ class NextMixinJs{
                         if( $v1.getAttribute('njs-filter-action') ){
                             $dataFilter = $v1.getAttribute('njs-filter-action');
                         }
-
-                        NextMixinJs.instance().openFilter($el, $dataFilter);
+                        $el.setAttribute('njs-active', $dataFilter);
                       }
 
                    });
@@ -184,7 +198,9 @@ class NextMixinJs{
 
         let $el = document.querySelector('[njs-mixin="'+$target+'"]');
         if($el){
-            NextMixinJs.instance().openFilter($el, $dataFilter);
+            $el.setAttribute('njs-active', $dataFilter);
+
+            NextMixinJs.instance().getContents($el);
         }
        return;
     }
@@ -202,26 +218,38 @@ class NextMixinJs{
         }
         var $totalHeight = 0;
         let $items = $itemCon.querySelectorAll($itemSelector);
+        let $targetClass = $target.replace('.', '').replace('#', '');
         if($items){
             $items.forEach(function($v, $k){
-                if($target != '*'){
+                if($targetClass != '*'){
+                   
+                    if($v.classList.contains($targetClass) ){
 
-                    let $clientItem = $v.getBoundingClientRect();
-                    let $height = ($clientItem.height) ? $clientItem.height: 0;
-                    let $width = ($clientItem.width) ? $clientItem.width: 0;
+                        /*let $clientItem = $v.getBoundingClientRect();
+                        let $height = ($clientItem.height) ? $clientItem.height: 0;
+                        let $width = ($clientItem.width) ? $clientItem.width: 0;
+    
+                        $totalHeight += Math.floor($height + $gutter);*/
 
-
-
-                    $totalHeight += Math.floor($height + $gutter);
-                } 
+                        $v.style.display = 'block';
+                        console.log($target);
+                    } else {
+                        $v.style.display = 'none';
+                    }
+                    
+                } else {
+                    $v.style.display = 'block';
+                }
             });
+            
+                        
         }
-
+        
         if($totalHeight != 0 && $target != '*'){
             //$itemCon.style.height = ($totalHeight / $columns)  + 'px';
         }
     
-        //console.log($items);
+        
     }
 
     getContents( $el ){
@@ -229,14 +257,18 @@ class NextMixinJs{
        
         let $columns = ($settings.columns) ? $settings.columns : 3;
         let $type = ($settings.type) ? $settings.type : 'grid';
+        let $ratio = ($settings.ratio) ? $settings.ratio : 1;
         let $gutter = ($settings.gutter) ? $settings.gutter : 0;
         
 
         let $client = $el.getBoundingClientRect();
-        console.log($client);
-
+        
         var $tWidth = $client.width;
         var $tHeight = $client.height;
+
+        var $target = $el.getAttribute('njs-active');
+        let $targetClass = $target.replace('.', '').replace('#', '');
+        //console.log($targetClass);
 
         var $responMode = $el.getAttribute('njs-display');
         if($responMode == "tablate"){
@@ -245,9 +277,10 @@ class NextMixinJs{
         else if($responMode == "mobile"){
             $columns = ($settings.columnsMobile) ? $settings.columnsMobile : 1;
         }
+       
         let $gutterCOl = Math.floor($gutter * $columns);
         let $itemWidth = Math.floor(($tWidth - $gutterCOl) / $columns);
-        
+
         let $itemConSelector = ($settings.itemContainer) ? $settings.itemContainer : '.njs-item-container';
         let $itemSelector = ($settings.item) ? $settings.item : '.njs-item';
         let $itemHeightSelector = ($settings.itemHeight) ? $settings.itemHeight : '.njs-item-height';
@@ -259,66 +292,113 @@ class NextMixinJs{
         $itemCon.style.position = 'relative';
 
         var $totalHeight = 0;
-
+        let $heigthColumnCount = 0;
         let $items = $itemCon.querySelectorAll($itemSelector);
         if($items){
             let $top = 0;
             let $left = 0;
-            let $stepLeft = 0;
+            let $itemLeft = 0;
             let $stepTop = 0;
+
+            let $widthRatio = 1;
+            let $heightRatio = $ratio;
            
-            let $totalItem = Number($items.length);
+            let $totalItemLe = Number($items.length);
             let $Itemheight = 'auto';
+            let $start = 0;
+            let $totalItem = 1;
+            let $maxHeight = [];
             $items.forEach(function($v, $k){
-                
-                if($k % $columns != 0){
-                    $stepLeft += 1;
+
+                if($v.classList.contains($targetClass) || $targetClass == '*'){
+                    console.log($k);
+
+                    $v.style.display = 'block';
                 } else {
-                    $stepLeft = 0;
+                    $v.style.display = 'none';
                 }
 
-                if( ($k % $columns) == 0 && $k != 0){
-                    $stepTop += 1;
-                }
                 let $clientItem = $v.getBoundingClientRect();
-
                 let $height = Math.floor(($clientItem.height) ? $clientItem.height: 0);
                 let $width = ($clientItem.width) ? $clientItem.width: 0;
-                if($type == 'grid'){
-                   $Itemheight = Math.floor($itemWidth)  + 'px';
-                   $top = Math.floor(($itemWidth + $gutter) *  $stepTop);
-                   
-                   $totalHeight += Math.floor($itemWidth + $gutter);
-                } else {
-                   $top = Math.floor(($height + $gutter) *  $stepTop);
 
-                   $totalHeight += Math.floor($height + $gutter);
+                $widthRatio = Math.floor(($v.getAttribute('njs-width')) ? $v.getAttribute('njs-width') : 1);
+                $heightRatio = Math.floor(($v.getAttribute('njs-height')) ? $v.getAttribute('njs-height') : $ratio);
+               
+                if( $widthRatio > $columns){
+                    $widthRatio = $columns;
                 }
+               
+                // set width
+                var $itWidth = Math.floor( ($itemWidth * $widthRatio) + ($gutter * ($widthRatio - 1)) );
                 
                 // set height
+                if($type == 'grid'){
+                    $Itemheight = Math.floor($itemWidth * $heightRatio)  + 'px';
+                    $top = Math.floor( ($itemWidth * $heightRatio) + $gutter);
+                } else {
+                    $top = Math.floor( ($height * $heightRatio) + $gutter);
+                    $Itemheight = Math.floor($height + $gutter)  + 'px';
+                }
+                $maxHeight.push($top);
+
                 let $heightItem = $v.querySelector($itemHeightSelector);
                 if( !$heightItem ){
                     $heightItem = $v;
                 }
                 $heightItem.style.height = $Itemheight;
-               
                 
-                $left = parseInt( ($itemWidth + $gutter) * Number($stepLeft));
-               
-                console.log($itemWidth);
-                $v.style.width = $itemWidth + 'px';
+                $v.style.width =  $itWidth + 'px';
                 $v.style.position = 'absolute';
-                $v.style.top = $top + 'px';
-                $v.style.left = $left + 'px';
+                $v.style.top = $stepTop + 'px';
+                $v.style.left = $itemLeft + 'px';
+                $v.setAttribute('njs-index-item', $totalItem);
 
-               
+                $start += $widthRatio;
+                let $avg = Math.floor($start % $columns);
                 
+                if($avg == 0){
+                    $stepTop += NextMixinJs.instance().maxValue($maxHeight);
+                    $itemLeft = 0;
+                    $start = 0;
+                    $maxHeight = [];
+                } else{
+                    $itemLeft += $itWidth + $gutter;
+                    let $e = ($items[$totalItem]) ? $items[$totalItem] : '';
+                    if( $e ){
+                        let $next = Math.floor(($e.getAttribute('njs-width')) ? $e.getAttribute('njs-width') : 1);
+                        let $nextTotal = Number($start + $next);
+                        //console.log($nextTotal + ' max ' +$totalItem);
+                        if( $nextTotal > $columns){
+                            $stepTop += NextMixinJs.instance().maxValue($maxHeight);
+                            $itemLeft = 0;
+                            $start = 0;
+                            $maxHeight = [];
+                        } 
+                    } else {
+                        $stepTop += NextMixinJs.instance().maxValue($maxHeight);
+                    } 
+                }
+                
+                $totalHeight = Math.floor($stepTop) ;
+                $totalItem++;
+               
             });
         }
-
-        $itemCon.style.height = ($totalHeight / $columns)  + 'px';
-
+        
+        $itemCon.style.height = '100%';
+        $itemCon.style.minHeight = Math.floor($totalHeight)  + 'px';
     }
+
+    maxValue(arr) {
+        var len = arr.length, max = -1;
+        while (len--) {
+          if (arr[len] > max) {
+            max = arr[len];
+          }
+        }
+        return max;
+    };
 
     // instance of class
     static instance() {
