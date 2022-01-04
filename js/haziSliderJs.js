badfcfed1a0e3697234b9f6ead4e5376a6c1e9da
dev-hazi <hazi.droitlab@gmail.com>
2022-01-04 17:53:39 +0600

    slider

--- js/haziSliderJs.js
+++ js/haziSliderJs.js
@@ -28,7 +28,7 @@ var $hzslider = {
             });
             return $slider;
         }
-        return false;
+        return $hzslider;
     },
     renderSlider: function( $v ){
         if( !$v ){
@@ -53,6 +53,8 @@ var $hzslider = {
             $speed = ($sett.speed) ? $sett.speed : 1000,
             $duration = ($sett.duration) ? $sett.duration : 300,
             $autoplay = ($sett.autoplay) ? $sett.autoplay : {},
+            $navigation = ($sett.navigation) ? $sett.navigation : { container : true },
+            $pagination = ($sett.pagination) ? $sett.pagination : { container : true },
             $direction = ($sett.direction) ? $sett.direction : 'horizontal',
             $item = ($sett.itemSelector) ? $sett.itemSelector : '.hzslider-slide',
             $itemsEl = ($sett.itemsEl) ? $sett.itemsEl : $v.querySelectorAll($item),
@@ -119,31 +121,75 @@ var $hzslider = {
 
             // autoplay start
             if( $autoplay.enable ){
-                setInterval(() => {
-                    
-                    $hzslider.resetClass($itemsEl);
-                    let $setIndex = Number($v.getAttribute('hjs-start')) + Math.abs($step);
-                    $setIndex = ( $itemsTotal < $setIndex) ? 1 : $setIndex;
-
-                    var $pointer = (($setIndex - 1) * ($iwidth + $spaceBetween) );
-                    if( $direction == 'vertical'){
-                        $pointer = (($setIndex - 1) * ($iheight + $spaceBetween));
-                        $v.style.transform = "translate3d(0px, -" + $pointer + "px, 0px)";
-                    }else{
-                        $v.style.transform = "translate3d(-" + $pointer + "px, 0px, 0px)";
-                    }
-                    $v.setAttribute('hjs-start', $setIndex);
-
-                    $hzslider.setPrev($itemsEl, $setIndex);
-                    if( $itemsEl[ $setIndex ]){
-                        $itemsEl[ $setIndex ].classList.add('hzslide-active');
-                    }
-                    $hzslider.setNext($itemsEl, $setIndex);
-
+                var $autoInterval = setInterval(() => {
+                    $hzslider.slideContinue( $v, 'next');
                 }, $speed);
             }
             // end autoplay
+            // start navigation code
+            let $conner = ($navigation.container) ? $navigation.container : true;
+            if( $navigation.nextEl ){
+                let $nxbtn = document.querySelector( $navigation.nextEl );
+                if( $conner ){
+                    $nxbtn = $parentEl.querySelector( $navigation.nextEl );
+                }
+                if( $nxbtn ){
+                    $nxbtn.setAttribute('aria-label', 'Next Slide');
+                    $nxbtn.setAttribute('hjs-control', $idSlide);
+                    $nxbtn.setAttribute('hjs-index', 'next');
+                    $nxbtn.addEventListener('click', function( $e ){
+                        $hzslider.slideContinue( $v, 'next');
+                        if( $autoplay.enable ){
+                            clearInterval( $autoInterval );
+                        }
+                    });
+                }
+            }
 
+            if( $navigation.prevEl ){
+                let $prebtn = document.querySelector( $navigation.prevEl );
+                if( $conner ){
+                    $prebtn = $parentEl.querySelector( $navigation.prevEl );
+                }
+                if( $prebtn ){
+                    $prebtn.setAttribute('aria-label', 'Previous slide');
+                    $prebtn.setAttribute('hjs-control', $idSlide);
+                    $prebtn.setAttribute('hjs-index', 'prev');
+                    $prebtn.addEventListener('click', function( $e ){
+                        $hzslider.slideContinue( $v, 'prev');
+                        if( $autoplay.enable ){
+                            clearInterval( $autoInterval );
+                        }
+                    });
+                }
+            }
+
+            // pagination
+            let $paginaCon = ($pagination.container) ? $pagination.container : true;
+            let $clickable = ($pagination.clickable) ? $pagination.clickable : true;
+            if( $pagination.el ){
+                let $pagi = document.querySelector( $pagination.el );
+                if( $paginaCon ){
+                    $pagi = $parentEl.querySelector( $pagination.el );
+                }
+
+                if( $pagi ){
+                    let $mode = ($pagination.mode) ? $pagination.mode : 'horizontal';
+                    $pagi.classList.add('hzslider-pagination-bullets', 'hzslider-pagination-' + $mode);
+                    if( !$pagi.querySelector( '*')){
+                        let $i = 1;
+                        for(; $i <= $itemsTotal; $i+= $step ){
+                            //console.log( $i );
+                            let $span = document.createElement('span');
+                            $span.setAttribute('class', 'hzslider-pagination-bullet hzbullet-' + $i);
+                            $span.setAttribute('hjs-control', $idSlide);
+                            $span.setAttribute('hjs-index', $i);
+                            $span.setAttribute('aria-label', $i + ' / ' + $itemsTotal);
+                            $pagi.appendChild($span);
+                        }
+                    }
+                }
+            }
         }
         
 
@@ -169,8 +215,62 @@ var $hzslider = {
         }
         return true;
     },
-    slideContinue: function($itemsEl, $index){
+    slideContinue: function($v, $typeSlide){
+        if( !$v ){
+            return;
+        }
+        let $parentEl = $v.parentElement;
+        // set Settings
+        let $offset = $parentEl.getBoundingClientRect(),
+            $width = $offset.width,
+            $height = $offset.height;
 
+        let $sett = $hzslider.getSettings( $v );
+        var $type = ($sett.type) ? $sett.type : 'none',
+            $step = ($sett.step) ? $sett.step : false,
+            $direction = ($sett.direction) ? $sett.direction : 'horizontal',
+            $slidesPerView = ($sett.slidesPerView) ? $sett.slidesPerView : 1,
+            $spaceBetween = ($sett.spaceBetween) ? $sett.spaceBetween : 10,
+            $item = ($sett.itemSelector) ? $sett.itemSelector : '.hzslider-slide',
+            $itemsEl = ($sett.itemsEl) ? $sett.itemsEl : $v.querySelectorAll($item),
+            $itemsTotal = $itemsEl.length;
+
+        var $iwidth = Math.floor( $width / Number($slidesPerView) );
+        var $iheight = 0;
+        if( $direction == 'vertical'){
+            $iwidth = 0;
+            $iheight = Math.floor( $height / Number($slidesPerView) );
+        }  
+        let $setIndex = 0;
+        $hzslider.resetClass($itemsEl);
+        if( $typeSlide == 'next' ){
+            $setIndex = Number($v.getAttribute('hjs-start')) + Math.abs($step);
+        } else if( $typeSlide == 'prev' ){
+            $setIndex = Number($v.getAttribute('hjs-start')) - Math.abs($step);
+        } else{
+            $setIndex = $typeSlide;
+        }
+        
+        $setIndex = ( $itemsTotal < $setIndex) ? 1 : $setIndex;
+        if( 0 >=  $setIndex ){
+            $setIndex = $itemsTotal;
+        } 
+        var $pointer = (($setIndex - 1) * ($iwidth + $spaceBetween) );
+        if( $direction == 'vertical'){
+            $pointer = (($setIndex - 1) * ($iheight + $spaceBetween));
+            $v.style.transform = "translate3d(0px, -" + $pointer + "px, 0px)";
+        }else{
+            $v.style.transform = "translate3d(-" + $pointer + "px, 0px, 0px)";
+        }
+        $v.setAttribute('hjs-start', $setIndex);
+
+        $hzslider.setPrev($itemsEl, $setIndex);
+        if( $itemsEl[ $setIndex ]){
+            $itemsEl[ $setIndex ].classList.add('hzslide-active');
+        }
+        $hzslider.setNext($itemsEl, $setIndex);
+
+        return $setIndex;
     },
     getSettings: function( $el ){
 
@@ -192,10 +292,13 @@ var $hzslider = {
             navigation : {
                 nextEl: ".hzslider-button-next",
                 prevEl: ".hzslider-button-prev",
+                container: true
             },
             pagination : {
                 el: ".hzslider-pagination",
                 clickable: true,
+                container: true,
+                mode: 'horizontal', // vertical, horizontal
             },
             type : 'none', // progressbar, fraction
         };
